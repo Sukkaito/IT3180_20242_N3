5c9f558b545022e95b2c868655964e6a722e4855
SonNguyen-25 <sonlienbat@gmail.com>
2025-05-15 09:41:11 +0700

    Add UserDTO and UserRepository

--- backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/repository/FineRepository.java
+++ backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/repository/FineRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import vn.edu.hust.nmcnpm_20242_n3.entity.BookLoan;
 import vn.edu.hust.nmcnpm_20242_n3.entity.Fine;
 
 import java.util.List;
