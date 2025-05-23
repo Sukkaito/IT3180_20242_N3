71eef0f5d7e96427e6b57808b64dc7a8f7e3a1a4
SonNguyen-25 <sonlienbat@gmail.com>
2025-05-23 11:19:35 +0700

    Add UserDTO and UserRepository

--- backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/entity/Permission.java
+++ backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/entity/Permission.java
@@ -3,8 +3,6 @@ import lombok.Getter;
 import jakarta.persistence.*;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
 @Entity
@@ -15,6 +13,4 @@ public class Permission {
     int id;
     @Column(unique = true, nullable = false)
     String name;
-    @ManyToMany(mappedBy = "permissions")
-    private Set<Role> roles;
 }
