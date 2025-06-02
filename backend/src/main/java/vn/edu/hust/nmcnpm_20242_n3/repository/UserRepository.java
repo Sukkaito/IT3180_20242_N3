7e02dc4f9bf30d34f7d7bce6360680fdf58dad06
SonNguyen-25 <sonlienbat@gmail.com>
2025-06-02 23:32:36 +0700

    Add UserDTO and UserRepository

--- backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/repository/UserRepository.java
+++ backend/src/main/java/vn/edu/hust/nmcnpm_20242_n3/repository/UserRepository.java
@@ -3,6 +3,7 @@ package vn.edu.hust.nmcnpm_20242_n3.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.hust.nmcnpm_20242_n3.entity.User;
 import vn.edu.hust.nmcnpm_20242_n3.constant.RoleEnum;
@@ -12,10 +13,18 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-    @Query("SELECT u FROM User u WHERE u.role.name = :role ORDER BY u.createdAt ASC")
+    @Query("SELECT u FROM User u WHERE u.role.name = :role ORDER BY u.CreatedAt ASC")
     List<User> findByRole_Name(RoleEnum role);
     Optional<User> findByEmail(String email);
     Optional<User> findByUserName(String userName);
     boolean existsByEmail(String email);
     boolean existsByUserName(String userName);
+    @Query("SELECT u FROM User u " +
+            "WHERE (:id IS NULL OR u.id LIKE %:id%) " +
+            "AND (:email IS NULL OR u.email LIKE %:email%) " +
+            "AND (:username IS NULL OR u.userName LIKE %:username%)")
+    List<User> searchUsers(@Param("id") String id,
+                           @Param("email") String email,
+                           @Param("username") String username);
+
 }
